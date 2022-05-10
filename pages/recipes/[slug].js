794e80c747d7c23afb40a54f176bb109ecb6a0a9
Blackchickenn <marcel.madaj@gmail.com>
2022-05-10 10:36:06 +0200

    added slug subpage connected with contentful

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,6 @@
 import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = createClient({
   //*Access to Contentful API via env local (w/o env locals they can be showed on Github)
@@ -25,8 +27,69 @@ export const getStaticPaths = async () => {
   };
 };
 
-export const getStaticProps = async () => {};
+export const getStaticProps = async ({ params }) => {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+  return {
+    props: { recipe: items[0] },
+  };
+};
 
-export default function RecipeDetails() {
-  return <div>Recipe Details</div>;
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
+  const { featuredImage, title, cookingTime, ingredients, method } =
+    recipe.fields;
+  return (
+    <div>
+      <div className="banner">
+        <Image
+          src={"https:" + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{title}</h2>
+      </div>
+      <div className="info">
+        <p>Take about {cookingTime} mins to cook</p>
+        <h3>
+          Ingredients:
+          {ingredients.map((ing) => (
+            <span key={ing}>{ing}</span>
+          ))}
+        </h3>
+      </div>
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+      <style jsx>{`
+        h2,
+        h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+      `}</style>
+    </div>
+  );
 }
